c25a6566515346049204734628bb27aae8c1a8ce
Miloni <milonisangani15@gmail.com>
2021-01-14 18:53:18 +0530

    sign_up_done

--- src/components/DisplayPlant.js
+++ src/components/DisplayPlant.js
@@ -1,29 +1,28 @@
 import React, {useState } from 'react';
-import {Jumbotron, Card, CardText, Button, ListGroupItem, ListGroup } from 'reactstrap';
+import {Jumbotron, Card, CardText, CardBody,CardTitle,CardSubtitle, Button, ListGroupItem, ListGroup } from 'reactstrap';
 
 const DisplayPlant = (props) =>{
   console.log("This works");
    return(
     
     <>
-         <p>{props.name} {props.desc}</p>
-    {/* <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
+    <Jumbotron>
     <p>{props.name} {props.desc}</p>
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card> */}
+    </Jumbotron>
+         
+      <div>
+      <Card>
+        
+        <CardBody>
+          <CardTitle tag="h5">Card title</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+          <p>{props.name} {props.desc}</p>
+        </CardBody>
+      </Card>
+    </div>
+   
     </>
      
       
