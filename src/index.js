b9687f13a8232c1d3a4bb7b89c8bb9ed88e08175
Rashmil Panchani <damonsalvatore833@gmail.com>
2021-01-14 18:35:14 +0530

    package changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import * as serviceWorker from "./serviceWorker";
 // Import the CSS libraries for the toast and the app
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
-import 'semantic-ui-css/semantic.min.css'
 
 import {toast,Zoom} from 'react-toastify';
 
