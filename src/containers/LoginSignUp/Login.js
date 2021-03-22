a75d8d36c2b023cb3a5c448e4ac827998a00f0ed
PRIYAL PATEL <vibhapriyal99@gmail.com>
2021-03-22 18:59:41 +0530

    changes made on landing page

--- src/containers/LoginSignUp/Login.js
+++ src/containers/LoginSignUp/Login.js
@@ -867,7 +867,7 @@ const Login = (props) => {
 
                 <div class="box hover:scale-2 relative rounded-3 py-1 mb-3 columns is-centered">
                     <div class="column column-12 md:column-4 mb-3 md:mb-0">
-                        <img class="" src="https://mudpi.app/svg/check_list.svg"></img>
+                        <img class=""style={{height:"120px"}} src="https://mudpi.app/svg/check_list.svg"></img>
                     </div>    
                     <div class="column column-12 md:column-8">
                         <h3 class="h4 text-primary">Gather Garden Insights</h3>
@@ -881,14 +881,14 @@ const Login = (props) => {
                         <img class="" src="https://mudpi.app/svg/sensor_stats.svg"></img>
                     </div>    
                     <div class="column column-12 md:column-8">
-                        <h3 class="h4 text-primary">Multi-Sensor Support</h3>
-                        <p class="mb-2">Gather useful data with a variety of sensors and components supported out of the box. Additional sensor and units can be integrated with the core system.</p> 
+                        <h3 class="h4 text-primary">Obtain Live Status Of Plants</h3>
+                        <p class="mb-2">An email will be sent on the registered email of the user with the live images of the plant.In this way the user can view their plants remotely. </p> 
                     </div> 
                 </div>
 
             </div>
             </div>
-            <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>    
+               
             <div class="mb-5 sm:mb-9">
             <div class="container"> 
             <div class="columns py-4 is-centered"> 
